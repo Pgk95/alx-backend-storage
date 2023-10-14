faa2516e8253e9a74591972056116df6bc9d7407
pgk95 <kalupgk@gmail.com>
2023-10-14 19:41:28 +0300

    quick fix

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,14 @@
 -- this script resets the attribute valid_email only,
 -- when the email has been changed
 
-CREATE OR REPLACE FUNCTION reset_valid_email()
-RETURNS TRIGGER AS $$
+DELIMITER $$
+CREATE TRIGGER reset
+BEFORE UPDATE ON users
+FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
-        NEW.valid_email = 0;
+        SET NEW.valid_email = 0;
     END IF;
-    RETURN NEW;
-END;
+END $$
+
+DELIMITER ;
