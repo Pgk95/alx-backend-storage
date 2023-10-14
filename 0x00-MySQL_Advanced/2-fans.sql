afd8af63462acf76cd1718d8d4441b5c7acc155c
pgk95 <kalupgk@gmail.com>
2023-10-14 11:04:53 +0300

    quickf ix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- ranks country origins of bands
 -- ordered by the number of (non-unique) fans
 
-SELECT country, COUNT(*) AS fan_count
+SELECT country, COUNT(*) AS nb_fans
 FROM metal_bands
-GROUP BY country
-ORDER BY fan_count DESC;
+GROUP BY origin
+ORDER BY nb_fans DESC;
