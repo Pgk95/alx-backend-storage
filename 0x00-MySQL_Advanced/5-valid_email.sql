783ea27a008671aefe23fa1f852a3d62651805da
pgk95 <kalupgk@gmail.com>
2023-10-14 19:30:44 +0300

    task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- this script resets the attribute valid_email only,
+-- when the email has been changed
+
+CREATE OR REPLACE FUNCTION reset_valid_email()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        NEW.valid_email := false;
+    END IF;
+    RETURN NEW;
+END;
