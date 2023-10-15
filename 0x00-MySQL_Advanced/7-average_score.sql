99b7f71e291c20649181ff0cf298e04f5ca1369d
pgk95 <kalupgk@gmail.com>
2023-10-15 10:19:26 +0300

    task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- creates a sorted procedure 'ComputeAverageScoreForUser'
+-- that computes the average score for a given student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE correction.user_id = user_id 
+    )
+    WHERE id = user_id;
+END$$
+DELIMITER ;
