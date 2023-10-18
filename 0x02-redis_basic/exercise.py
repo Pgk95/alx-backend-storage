21863aa0994a2a335881a0b08156cf052fb4009e
pgk95 <kalupgk@gmail.com>
2023-10-18 12:58:49 +0300

    quickf fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> any:
+    def store(self, data) -> str:
         """Store data in cache"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
