58164b2e35d85a5feabf2ab13f4228940b7789ef
pgk95 <kalupgk@gmail.com>
2023-10-18 12:30:56 +0300

    task 1

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Redis caching module"""
+
+import redis
+import uuid
+
+
+class Cache:
+    """Cache class"""
+
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> [str, bytes, int, float]:
+        """Store data in cache"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
